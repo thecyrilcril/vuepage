456b337ec8e1b223c4081f45033292d82524a082
Cyril Fehintoluwa <cyrilcril@gmail.com>
2023-12-31 21:41:29 +0100

    install font and extra setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +7,23 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            sans: ['Red Hat Display Variable', ...defaultTheme.fontFamily.sans],
+        },
+        colors: {
+          'v-blue': '#77AEF4',
+          'v-light-blue': '#D3E7FF',
+          'v-purple': '#2B2A3F',
+          'v-link': '#A3AED0',
+          'v-gray': '#f2f1f9',
+          'v-form-gray': '#f5f4fa',
+          'v-icon-purple': '#A3AED0',
+        },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
