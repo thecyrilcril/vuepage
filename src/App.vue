e4985e0398a164ab6263e0f0f1fb60d5842d5b7e
Cyril Fehintoluwa <cyrilcril@gmail.com>
2024-01-01 18:01:24 +0100

    refactor navigation to a component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 import { vOnClickOutside } from '@vueuse/components'
+import Navigation from '@/components/Navigation.vue'
 const userMenu = ref(false)
 </script>
 
 <template>
   <div class="grid grid-cols-[18.12rem_1fr] h-screen bg-v-gray">
-    <header class="bg-v-purple divide-y divide-gray-100/10 px-[2rem]">
-      <h1 class="flex justify-center my-[41px] items-center space-x-1">
-        <span class="relative flex justify-center">
-          <Union />
-          <span class="absolute text-[1.68rem] font-bold text-v-blue pt-1">W</span>
-
-        </span>
-        <span class="text-[1.68rem] font-bold text-white leading-[1.68rem] tracking-wide">WordPilot</span>
-      </h1>
-      <ul class="text-v-link space-y-2 pt-[2.93rem]">
-        <li>
-          <a href="#" class="group group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] font-bold hover:bg-v-blue hover:text-white active:font-bold">
-            <Dashboard class="w-6 h-6 group-hover:text-white" />
-            <span>Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
-            <Projects class="w-6 h-6 group-hover:text-white" />
-            <span>Projects</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
-            <Scheduler class="w-6 h-6 group-hover:text-white" />
-            <span>Scheduler</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
-            <ContentWizard class="w-6 h-6 group-hover:text-white" />
-            <span>Content Wizard</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
-            <Documents class="w-6 h-6 group-hover:text-white" />
-            <span>Documents</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
-            <LogOut class="w-6 h-6 group-hover:text-white" />
-            <span>Log Out</span>
-          </a>
-        </li>
-      </ul>
-    </header>
-
+    <Navigation />
     <main class="px-[1.62rem] pt-[1.75rem] pb-[1.93rem]">
       <div class="header bg-white rounded-[0.25rem] px-6 py-[18px] flex justify-between">
         <div class="flex space-x-6 items-center">
---
+++ src/components/Navigation.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="bg-v-purple divide-y divide-gray-100/10 px-[2rem]">
+      <h1 class="flex justify-center my-[41px] items-center space-x-1">
+        <span class="relative flex justify-center">
+          <Union />
+          <span class="absolute text-[1.68rem] font-bold text-v-blue pt-1">W</span>
+
+        </span>
+        <span class="text-[1.68rem] font-bold text-white leading-[1.68rem] tracking-wide">WordPilot</span>
+      </h1>
+      <ul class="text-v-link space-y-2 pt-[2.93rem]">
+        <li>
+          <a href="#" class="group group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] font-bold hover:bg-v-blue hover:text-white active:font-bold">
+            <Dashboard class="w-6 h-6 group-hover:text-white" />
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
+            <Projects class="w-6 h-6 group-hover:text-white" />
+            <span>Projects</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
+            <Scheduler class="w-6 h-6 group-hover:text-white" />
+            <span>Scheduler</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
+            <ContentWizard class="w-6 h-6 group-hover:text-white" />
+            <span>Content Wizard</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
+            <Documents class="w-6 h-6 group-hover:text-white" />
+            <span>Documents</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group flex space-x-3 rounded-[0.25rem] pl-[0.81rem] py-[0.69rem] h-[2.87rem] hover:bg-v-blue hover:text-white active:font-bold">
+            <LogOut class="w-6 h-6 group-hover:text-white" />
+            <span>Log Out</span>
+          </a>
+        </li>
+      </ul>
+    </header>
+</template>
